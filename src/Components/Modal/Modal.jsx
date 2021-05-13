44605e59a146b1a4d2832130e20122553b492fce
Ana Marelja <anamarelja@Anas-MacBook-Pro.local>
2021-05-13 20:44:40 +0200

    modal form added + validation

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,10 +1,10 @@
 import "./Modal.scss";
 import React, { useContext, useState } from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-import {themeContext} from '../../App'
-import TextField from '@material-ui/core/TextField';
+import { themeContext } from "../../App";
+import TextField from "@material-ui/core/TextField";
 import Modal from "@material-ui/core/Modal";
-import {validContext} from '../../App' 
+import { validContext } from "../../App";
 
 function getModalStyle() {
   const top = 50;
@@ -30,47 +30,89 @@ const useStyles = makeStyles((theme) =>
 );
 
 const MyModal = (props) => {
-
-  const {theme} = useContext(themeContext);
-  const {setValidData} = useContext(validContext);
+  const { theme } = useContext(themeContext);
+  const { setValidData } = useContext(validContext);
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
-  const [userEmail, setUserEmail] = useState('');
-  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState("");
+  const [userName, setUserName] = useState("");
 
   const handleClose = () => {
     props.openModal(false);
   };
 
   const submitUser = () => {
-    fetch('https://60965395116f3f00174b2f00.mockapi.io/users',{
-          method:"POST",
-          headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            name: userName,
-            email: userEmail,
-            image: 'https://primacgurus.com.au/wp-content/uploads/2021/01/No-Profile-image.jpg'
-        })
-        
+    fetch("https://60965395116f3f00174b2f00.mockapi.io/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail,
+        image:
+          "https://primacgurus.com.au/wp-content/uploads/2021/01/No-Profile-image.jpg",
+      }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        setValidData(false)
+        handleClose()
       })
-    .then((res) => res.json())
-    .then(() => setValidData(false))
-    .catch((error) => alert('Oops! Something went wrong... :( Please try again.'))
-  }
+      .catch((error) =>
+        alert("Oops! Something went wrong... :( Please try again.")
+      );
+  };
+
+  const submitForm = async (event) => {
+    event.preventDefault()
+    try{
+        await submitUser()
+    } catch(e){
+        alert('Oops! Something went wrong... :( Please try again.')
+    }
+}
 
   const body = (
-    <div style={modalStyle} className={`${classes.paper} modal-${theme} modal-view`}>
-      <h2 id="simple-modal-title" className="is-size-4 has-text-centered mb-3 main-text">Add New User</h2>
-      <p id="simple-modal-description" className="is-flex is-flex-direction-column is-align-items-center">
-      <TextField required id="standard-required standard-basic" label="Name" className="m-1 inputs" onChange={(e)=>setUserName(e.target.value)}/>
-      <TextField required id="standard-required standard-basic" label="Email" className="m-1 inputs" onChange={(e)=>setUserEmail(e.target.value)}/>
-      <button className="submit-button" onClick={()=>{
-          submitUser()
-          handleClose()
-      }}><i class="fas fa-paper-plane"></i> Sumbit</button>
-      </p>    
+    <div
+      style={modalStyle}
+      className={`${classes.paper} modal-${theme} modal-view`}
+    >
+      <h2
+        id="simple-modal-title"
+        className="is-size-4 has-text-centered mb-3 main-text"
+      >
+        Add New User
+      </h2>
+      <p
+        id="simple-modal-description"
+        className="is-flex is-flex-direction-column is-align-items-center"
+      >
+        <form onSubmit={submitForm}>
+          <TextField
+            required
+            id="standard-required standard-basic"
+            label="Name"
+            className="m-1 inputs"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <TextField
+            required
+            type="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            id="standard-required standard-basic"
+            label="Email"
+            className="m-1 inputs"
+            onChange={(e) => setUserEmail(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="submit-button"
+          >
+            <i class="fas fa-paper-plane"></i> Sumbit
+          </button>
+        </form>
+      </p>
     </div>
   );
 
